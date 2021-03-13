4ba4feb09947cd00abd2ff4100c3f5f868050c2c
Mike Le <mike.le@live.com>
2021-03-13 14:54:31 -0800

    initiated project and added axios to package

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
+import axios from 'axios';
 
 function App() {
+  const [ pokemonList, setPokemonList ] = useState([]);
+  
+  const clickHandler = (e) => {
+    axios.get('https://pokeapi.co/api/v2/pokemon/?limit=802')
+      .then( res => console.log(res))
+      .catch( err => console.log(err));
+
+    console.log("Thanks for clicking");
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <button onClick={ clickHandler }>Fetch pokemons</button>
+
+
     </div>
   );
 }
